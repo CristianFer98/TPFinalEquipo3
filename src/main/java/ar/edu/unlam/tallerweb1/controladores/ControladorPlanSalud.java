90223379aadb84eb9fb767503ce30d097ef27478
Cris <c.fernandez.melian@hotmail.com>
2021-11-19 21:34:39 -0300

    Cheto

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorPlanSalud.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorPlanSalud.java
@@ -1,21 +1,13 @@
 package ar.edu.unlam.tallerweb1.controladores;
 
-import javax.servlet.http.HttpServletRequest;
-import com.mercadopago.MercadoPago;
-import com.mercadopago.exceptions.MPConfException;
-import com.mercadopago.exceptions.MPException;
-import com.mercadopago.resources.Preference;
-import com.mercadopago.resources.datastructures.preference.Item;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import ar.edu.unlam.tallerweb1.modelo.cotizacionExistenteException;
 import ar.edu.unlam.tallerweb1.servicios.ServicioPlanSalud;
 
 @Controller
@@ -33,8 +25,6 @@ public class ControladorPlanSalud {
 		return new ModelAndView("planesMedicos");
 
 	}
-
-	
 
 
 
