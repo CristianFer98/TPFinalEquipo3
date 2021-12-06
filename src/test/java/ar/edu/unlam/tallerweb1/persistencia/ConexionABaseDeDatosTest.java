b31239457740ba274f6d7fc30da1450c58a86b42
Cris <c.fernandez.melian@hotmail.com>
2021-12-06 15:25:10 -0300

    travis 7

--- src/test/java/ar/edu/unlam/tallerweb1/persistencia/ConexionABaseDeDatosTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/persistencia/ConexionABaseDeDatosTest.java
@@ -1,22 +1,20 @@
-//package ar.edu.unlam.tallerweb1.persistencia;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.junit.Assert.*;
-//
-//import org.junit.Test;
-//import org.springframework.test.annotation.Rollback;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import com.mysql.cj.xdevapi.SessionFactory;
-//
-//public class ConexionABaseDeDatosTest extends SpringTest {
-//
-//	@Test
-//	@Transactional
-//	@Rollback
-//	public void testQueMeCompruebaConexionABD() {
-//		
-//        assertThat(session().isConnected()).isTrue();//conexion de base de datos exitosa
-//	}
-//
-//}
+package ar.edu.unlam.tallerweb1.persistencia;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+
+public class ConexionABaseDeDatosTest extends SpringTest {
+
+	@Test
+	@Transactional
+	@Rollback
+	public void testQueMeCompruebaConexionABD() {
+		
+        assertThat(session().isConnected()).isTrue();//conexion de base de datos exitosa
+	}
+
+}
